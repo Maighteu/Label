3770873029e6de092da91469e4e830996e5ddab7
Maight <145431868+Maighteu@users.noreply.github.com>
2024-04-18 14:41:14 +0200

    test commit

---
+++ src/Interface.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class Interface {
+    private JTextField userTextField;
+    private JPasswordField passwordPasswordField;
+    private JButton createAlbumButton;
+    private JTextField albumTextField;
+    private JTextField prixTextField;
+    private JTextField dateSortieTextField;
+    private JTextField numClientTextField;
+    private JTextField textField5;
+    private JButton connexionButton;
+    private JButton createClientButton;
+    private JComboBox comboBox1;
+    private JSpinner spinner1;
+    private JButton ajoutButton;
+    private JButton confirmerButton;
+    private JButton ajoutButton1;
+    private JTextField TItreTextField;
+    private JTextArea Album_OUT;
+    private JTextArea Groupe_OUT;
+    private JTextArea Prix_OUT;
+    private JTextArea Date_Sortie_OUT;
+    private JList Titre_List;
+    private JComboBox comboBox2;
+    private JTextField textField1;
+    private JButton ajoutArtisteButton;
+    private JTextField textField2;
+    private JButton createGroupeButton;
+    private JScrollBar scrollBar1;
+    private JScrollBar scrollBar2;
+    private JList list1;
+    private JTextArea Etat_Connexion;
+}
